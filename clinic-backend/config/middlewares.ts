734a621e28b2512d67bae5b0cfdafa115ab05b98
Dalu3 <daria.lysunets03@gmail.com>
2026-05-16 20:39:00 +0100

    [fix] allow R2 CDN images in Strapi admin CSP

--- clinic-backend/config/middlewares.ts
+++ clinic-backend/config/middlewares.ts
@@ -1,8 +1,38 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://pub-7bbc3c2ba7f44ae5865d2230f3c7f008.r2.dev',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://pub-7bbc3c2ba7f44ae5865d2230f3c7f008.r2.dev',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: '*',
+    },
+  },
   'global::api-cache',
   'strapi::poweredBy',
   'strapi::query',
